57eba652ea42e40b6654b07a1744b123c93f3e10
Richard Tzanov <richard.tzanov@sainsburys.co.uk>
2020-10-29 10:43:23 +0000

    socket emitting and receiving messages correctly

--- index.ts
+++ index.ts
@@ -1,19 +1,32 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
+import ioserver, { Socket } from 'socket.io';
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
 
+const server = require('http').Server(app);
 
-app.use(express.static('public'));
-app.get('*', (req, res) => {
-   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+const io = ioserver(server);
+
+io.on('connection', (socket: Socket) => {
+  console.log('a user connected');
+  socket.on('message', function (message: any) {
+    console.log(message);
+  });
 });
 
-app.listen(process.env.PORT, () => {
-   console.log(`Server is up at port ${process.env.PORT}`);
+app.use(cors());
+
+app.use(express.static('public'));
+app.get('*', (req: Request, res: Response) => {
+   console.log('sigh')
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server is up at port ${process.env.PORT}`);
 });
--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,47 @@
 <script>
-	export let name;
+	import io from 'socket.io-client';
+	const socket = io()
+  export let name;
+
+  const handleSubmit = () => {
+		console.log('sumbitted')
+    socket.emit(`message`, `whyyyyy`);
+  };
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+
+	button {
+		height: 20px;
+		width: 60px;
 	}
 </style>
+
+<main>
+  <h1>Hello asahjaash {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+  <button on:click={handleSubmit}>Press me</button>
+</main>
