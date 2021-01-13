decd7b5762eef60e078cd18bcdc43db1f9dc2955
Richard Tzanov <richard.tzanov@sainsburys.co.uk>
2021-01-13 14:04:26 +0000

    add RejoinPlayer

---
+++ src/components/GameRoom/RejoinPlayer.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { mutation } from 'svelte-apollo';
+  import { REJOIN_PLAYER } from '../../gql';
+  import Button from '../common/Button.svelte';
+  export let joined;
+  export let player;
+  export let gameId;
+
+  const rejoinPlayer = mutation(REJOIN_PLAYER);
+  const handleRejoin = async () => {
+    await rejoinPlayer({
+      variables: { gameId, player },
+    });
+    joined();
+  };
+</script>
+
+<Button onClick={handleRejoin}>Rejoin game</Button>
